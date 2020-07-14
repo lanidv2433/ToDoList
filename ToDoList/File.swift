263da2b83ae09bc564da99aa2c2203afbcbc4544
lanidv2433 <lanidv2433@gmail.com>
2020-07-14 11:56:53 -0400

    To Do List demo

---
+++ ToDoList/File.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  ToDoList
+//
+//  Created by Lan-Anh Dang-Vu on 7/14/20.
+//  Copyright © 2020 Lan-Anh Dang-Vu. All rights reserved.
+//
+
+import UIKit
+class ToDo{
+   var name = ""
+    var important = false
+}
+
